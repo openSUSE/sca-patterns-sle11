652a45276b0b38dce14598407779d6049d37292e
Jason Record <g23guy@gmail.com>
2014-03-31 14:51:07 -0600

    change to perl

--- patterns/SLE/sle11sp3/oops-7014823.pl
+++ patterns/SLE/sle11sp3/oops-7014823.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/perl
 
 # Title:       Detect kernel oops in 3.0.101-0.18.1
 # Description: kernel Oops/BUG during network operation
@@ -28,48 +28,39 @@
 ##############################################################################
 # Module Definition
 ##############################################################################
-
-import sys, os, Core, SUSE
+use strict;
+use warnings;
+use SDP::Core;
+use SDP::SUSE;
 
 ##############################################################################
 # Overriden (eventually or in part) from SDP::Core Module
 ##############################################################################
 
-META_CLASS = "SLE"
-META_CATEGORY = "Kernel"
-META_COMPONENT = "Oops"
-PATTERN_ID = os.path.basename(__file__)
-PRIMARY_LINK = "META_LINK_TID"
-OVERALL = Core.TEMP
-OVERALL_INFO = "NOT SET"
-OTHER_LINKS = "META_LINK_TID=https://www.suse.com/support/kb/doc.php?id=7014823|META_LINK_BUG=https://bugzilla.novell.com/show_bug.cgi?id=870801"
-
-Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
-
-##############################################################################
-# Local Function Definitions
-##############################################################################
-
-def checkSomething():
-	fileOpen = "filename.txt"
-	section = "CommandToIdentifyFileSection"
-	content = {}
-	if Core.getSection(fileOpen, section, content):
-		for line in content:
-			if "something" in content[line]:
-				return True
-	return False
+@PATTERN_RESULTS = (
+"META_CLASS=SLE",
+"META_CATEGORY=Kernel",
+"META_COMPONENT=Oops",
+"PATTERN_ID=$PATTERN_ID",
+"PRIMARY_LINK=META_LINK_TID",
+"OVERALL=$GSTATUS",
+"OVERALL_INFO=NOT SET",
+"META_LINK_TID=https://www.suse.com/support/kb/doc.php?id=7014823",
+"META_LINK_BUG=https://bugzilla.novell.com/show_bug.cgi?id=870801",
+);
 
 ##############################################################################
 # Main Program Execution
 ##############################################################################
 
-KERNEL_VERSION = '3.0.101-0.18'
-INSTALLED_VERSION = SUSE.compareKernel(KERNEL_VERSION)
-if( INSTALLED_VERSION == 0 ):
-	Core.updateStatus(Core.CRIT, "Kernel susceptible to oops on network operations, update system to apply newest kernel")
-else:
-	Core.updateStatus(Core.IGNORE, "Bug fixes applied, not applicable to installed kernel")
+my $AFFECTED_KERNEL = "3.0.101-0.18";
 
-Core.printPatternResults()
+SDP::Core::processOptions();
+if ( SDP::SUSE::compareKernel($AFFECTED_KERNEL) == 0 ) {
+	SDP::Core::updateStatus(STATUS_CRITICAL, "Kernel susceptible to oops on network operations, update system to apply newest kernel");
+} else {
+	SDP::Core::updateStatus(STATUS_IGNORE, "Bug fixes applied, not applicable to installed kernel");
+}
+SDP::Core::printPatternResults();
+exit;
 
