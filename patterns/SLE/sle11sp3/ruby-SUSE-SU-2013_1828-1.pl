1a847bba6d287196511cefc9c8eaa259d2bc38bc
Jason Record <g23guy@gmail.com>
2014-04-02 11:48:36 -0600

    new patterns

---
+++ patterns/SLE/sle11sp3/ruby-SUSE-SU-2013_1828-1.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+# Title:       Ruby SA Critical SUSE-SU-2013:1828-1
+# Description: fixes two vulnerabilities
+# Modified:    2014 Apr 02
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+##############################################################################
+# Module Definition
+##############################################################################
+use strict;
+use warnings;
+use SDP::Core;
+use SDP::SUSE;
+
+##############################################################################
+# Overriden (eventually or in part) from SDP::Core Module
+##############################################################################
+
+@PATTERN_RESULTS = (
+"META_CLASS=Security",
+"META_CATEGORY=SLE",
+"META_COMPONENT=Ruby",
+"PATTERN_ID=$PATTERN_ID",
+"PRIMARY_LINK=META_LINK_Security",
+"OVERALL=$GSTATUS",
+"OVERALL_INFO=NOT SET",
+"META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2013-12/msg00001.html",
+);
+
+##############################################################################
+# Main Program Execution
+##############################################################################
+
+SDP::Core::processOptions();
+my $NAME = 'Ruby';
+my $MAIN_PACKAGE = 'ruby';
+my $SEVERITY = 'Important';
+my $TAG = 'SUSE-SU-2013:1828-1';
+my %PACKAGES = ();
+if ( SDP::SUSE::compareKernel(SLE11SP2) >= 0 && SDP::SUSE::compareKernel(SLE11SP4) < 0 ) {
+	%PACKAGES = (
+		'ruby' => '1.8.7.p357-0.9.13.1',
+		'ruby-devel' => '1.8.7.p357-0.9.13.1',
+		'ruby-doc-html' => '1.8.7.p357-0.9.13.1',
+		'ruby-doc-ri' => '1.8.7.p357-0.9.13.1',
+		'ruby-examples' => '1.8.7.p357-0.9.13.1',
+		'ruby-test-suite' => '1.8.7.p357-0.9.13.1',
+		'ruby-tk' => '1.8.7.p357-0.9.13.1',
+	);
+	SDP::SUSE::securityAnnouncementPackageCheck($NAME, $MAIN_PACKAGE, $SEVERITY, $TAG, \%PACKAGES);
+} else {
+	SDP::Core::updateStatus(STATUS_ERROR, "ERROR: $NAME Security Announcement: Outside the kernel scope");
+}
+SDP::Core::printPatternResults();
+
+exit;
+
