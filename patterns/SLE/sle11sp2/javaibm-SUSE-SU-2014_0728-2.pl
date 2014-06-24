ace441e036b08a3eab176021b0c02638e3e3c67f
Jason Record <g23guy@gmail.com>
2014-06-24 15:07:07 -0600

    new patterns

---
+++ patterns/SLE/sle11sp2/javaibm-SUSE-SU-2014_0728-2.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+
+# Title:       IBM Java 1.6.0 SA Important SUSE-SU-2014:0728-2
+# Description: fixes 26 vulnerabilities
+# Modified:    2014 Jun 24
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+##############################################################################
+# Module Definition
+##############################################################################
+use strict;
+use warnings;
+use SDP::Core;
+use SDP::SUSE;
+
+##############################################################################
+# Overriden (eventually or in part) from SDP::Core Module
+##############################################################################
+
+@PATTERN_RESULTS = (
+"META_CLASS=Security",
+"META_CATEGORY=SLE",
+"META_COMPONENT=Java",
+"PATTERN_ID=$PATTERN_ID",
+"PRIMARY_LINK=META_LINK_Security",
+"OVERALL=$GSTATUS",
+"OVERALL_INFO=NOT SET",
+"META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-05/msg00018.html",
+);
+
+##############################################################################
+# Main Program Execution
+##############################################################################
+
+SDP::Core::processOptions();
+my $NAME = 'LTSS IBM Java';
+my $MAIN_PACKAGE = '';
+my $SEVERITY = 'Important';
+my $TAG = 'SUSE-SU-2014:0728-2';
+my %PACKAGES = ();
+if ( SDP::SUSE::compareKernel(SLE11SP2) >= 0 && SDP::SUSE::compareKernel(SLE11SP3) < 0 ) {
+	%PACKAGES = (
+		'java-1_6_0-ibm' => '1.6.0_sr16.0-0.3.1',
+		'java-1_6_0-ibm-alsa' => '1.6.0_sr16.0-0.3.1',
+		'java-1_6_0-ibm-devel' => '1.6.0_sr16.0-0.3.1',
+		'java-1_6_0-ibm-fonts' => '1.6.0_sr16.0-0.3.1',
+		'java-1_6_0-ibm-jdbc' => '1.6.0_sr16.0-0.3.1',
+		'java-1_6_0-ibm-plugin' => '1.6.0_sr16.0-0.3.1',
+	);
+	SDP::SUSE::securityAnnouncementPackageCheck($NAME, $MAIN_PACKAGE, $SEVERITY, $TAG, \%PACKAGES);
+} else {
+	SDP::Core::updateStatus(STATUS_ERROR, "ERROR: $NAME Security Announcement: Outside the kernel scope");
+}
+SDP::Core::printPatternResults();
+
+exit;
+
