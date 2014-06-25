2d7110f98c380a04ab18e950cc9de9f6c17e129b
Jason Record <g23guy@gmail.com>
2014-06-25 14:35:03 -0600

    sle11sp3/flash-SUSE-SU-2014_0806-1.pl

---
+++ patterns/SLE/sle11sp3/flash-SUSE-SU-2014_0806-1.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+
+# Title:       Flash Player SA Important SUSE-SU-2014:0806-1
+# Description: fixes 6 vulnerabilities
+# Modified:    2014 Jun 25
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+##############################################################################
+# Module Definition
+##############################################################################
+use strict;
+use warnings;
+use SDP::Core;
+use SDP::SUSE;
+
+##############################################################################
+# Overriden (eventually or in part) from SDP::Core Module
+##############################################################################
+
+@PATTERN_RESULTS = (
+"META_CLASS=Security",
+"META_CATEGORY=SLE",
+"META_COMPONENT=Flash Player",
+"PATTERN_ID=$PATTERN_ID",
+"PRIMARY_LINK=META_LINK_Security",
+"OVERALL=$GSTATUS",
+"OVERALL_INFO=NOT SET",
+"META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-06/msg00021.html",
+);
+
+##############################################################################
+# Main Program Execution
+##############################################################################
+
+SDP::Core::processOptions();
+my $NAME = 'Flash Player';
+my $MAIN_PACKAGE = 'flash-player';
+my $SEVERITY = 'Important';
+my $TAG = 'SUSE-SU-2014:0806-1';
+my %PACKAGES = ();
+if ( SDP::SUSE::compareKernel(SLE11SP3) >= 0 && SDP::SUSE::compareKernel(SLE11SP4) < 0 ) {
+	%PACKAGES = (
+		'flash-player' => '11.2.202.378-0.3.1',
+		'flash-player-gnome' => '11.2.202.378-0.3.1',
+		'flash-player-kde4' => '11.2.202.378-0.3.1',
+	);
+	SDP::SUSE::securityAnnouncementPackageCheck($NAME, $MAIN_PACKAGE, $SEVERITY, $TAG, \%PACKAGES);
+} else {
+	SDP::Core::updateStatus(STATUS_ERROR, "ERROR: $NAME Security Announcement: Outside the kernel scope");
+}
+SDP::Core::printPatternResults();
+
+exit;
+
