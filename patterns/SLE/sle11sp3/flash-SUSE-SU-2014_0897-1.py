17c04a09108924394036e1b5f13e18ab0543931e
Jason Record <g23guy@gmail.com>
2014-07-17 16:18:17 -0600

    sle11sp3/flash-SUSE-SU-2014_0897-1.py

---
+++ patterns/SLE/sle11sp3/flash-SUSE-SU-2014_0897-1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+# Title:       Flash Player SA Critical SUSE-SU-2014:0897-1
+# Description: fixes three vulnerabilities
+# Modified:    2014 Jul 17
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+##############################################################################
+# Module Definition
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+##############################################################################
+# Overriden (eventually or in part) from SDP::Core Module
+##############################################################################
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "Flash Player"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-07/msg00007.html"
+
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+##############################################################################
+# Main Program Execution
+##############################################################################
+
+LTSS = False
+NAME = 'Flash Player'
+MAIN = 'flash-player'
+SEVERITY = 'Critical'
+TAG = 'SUSE-SU-2014:0897-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11 and SERVER['DistroPatchLevel'] == 3 ):
+	PACKAGES = {
+		'flash-player': '11.2.202.394-0.3.1',
+		'flash-player-gnome': '11.2.202.394-0.3.1',
+		'flash-player-kde4': '11.2.202.394-0.3.1',
+	}
+	SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+
+Core.printPatternResults()
+
