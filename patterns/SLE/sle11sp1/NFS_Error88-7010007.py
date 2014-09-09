517ddfce3f8ca96458f0ebbdd303adc20aad9e57
Jason Record <g23guy@gmail.com>
2014-09-09 17:07:48 -0600

    new patterns

---
+++ patterns/SLE/sle11sp1/NFS_Error88-7010007.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+# Title:       NFS transfers failing with RPC error 88 (ENOTSOCK)
+# Description: NFS transfers failing with RPC error 88 (ENOTSOCK) SLES 11 sp1 issue patch in kernel 2.6.32.54-0.3.1.
+# Modified:    2014 Sep 05
+#version 0.2
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Sean Barlow (sbarlow@novell.com)
+#
+##############################################################################
+
+##############################################################################
+# Module Definition
+#############################################################################
+
+import os
+import Core
+import SUSE
+
+##############################################################################
+# Overriden (eventually or in part) from SDP::Core Module
+##############################################################################
+META_CLASS = "SLE"
+META_CATEGORY = "Network"
+META_COMPONENT = "NFS"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_TID"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_TID=https://www.novell.com/support/kb/doc.php?id=7010007|META_LINK_BUG=https://bugzilla.novell.com/show_bug.cgi?id=733146"
+
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+##############################################################################
+# Local Function Definitions
+##############################################################################
+
+def checkMessages():
+	fileOpen = "messages.txt"
+	section = "/var/log/warn"
+	content = {}
+	if Core.getSection(fileOpen, section, content):
+		for line in content:
+			if "nfs: RPC call returned error 88" in content[line]:
+				return True
+	return False
+
+        section = "/var/log/messages"
+        content = {}
+        if Core.getSection(fileOpen, section, content):
+                for line in content:
+                        if "nfs: RPC call returned error 88" in content[line]:
+                                return True
+        return False
+
+##############################################################################
+# Main Program Execution
+##############################################################################
+KERNEL_VERSION = '2.6.32.54-0.3.1'
+INSTALLED_VERSION = SUSE.compareKernel(KERNEL_VERSION)
+if( INSTALLED_VERSION < 0 ):
+        Core.updateStatus(Core.WARN, "\"RPC call returned error 88\" bug detected in kernel version " + str(INSTALLED_VERSION) + " or before, update server for fixes")
+	if( checkMessages() ):
+		Core.updateStatus(Core.CRIT, "\"RPC call returned error 88\" bug detected in kernel version " + str(INSTALLED_VERSION) + " or before, update server for fixes")
+	else: Core.updateStatus(Core.IGNORE, "Bug fixes applied for " + KERNEL_VERSION)
+else:
+        Core.updateStatus(Core.IGNORE, "Ignore this pattern, not applicable")
+Core.printPatternResults()
+
---
+++ patterns/SLE/sle11sp3/MySQL_SUSE-SU-2014_1072-1_11.3.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for MySQL SUSE-SU-2014:1072-1
+# Description: Security fixes for SUSE Linux Enterprise 11 SP3
+# Source:      Security Announcement Parser v1.0.1
+# Modified:    2014 Sep 09
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "MySQL"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-08/msg00012.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'MySQL'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2014:1072-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11):
+	if ( SERVER['DistroPatchLevel'] == 3 ):
+		PACKAGES = {
+			'libmysqlclient_r15-x86': '5.0.96-0.6.13',
+			'mysql-tools': '5.5.39-0.7.1',
+			'libmysql55client_r18-32bit': '5.5.39-0.7.1',
+			'libmysql55client18-x86': '5.5.39-0.7.1',
+			'mysql': '5.5.39-0.7.1',
+			'mysql-client': '5.5.39-0.7.1',
+			'libmysqlclient15': '5.0.96-0.6.13',
+			'libmysqlclient15-x86': '5.0.96-0.6.13',
+			'libmysqlclient_r15-32bit': '5.0.96-0.6.13',
+			'libmysql55client18': '5.5.39-0.7.1',
+			'libmysql55client_r18': '5.5.39-0.7.1',
+			'libmysql55client_r18-x86': '5.5.39-0.7.1',
+			'libmysql55client18-32bit': '5.5.39-0.7.1',
+			'libmysqlclient_r15': '5.0.96-0.6.13',
+			'libmysqlclient15-32bit': '5.0.96-0.6.13',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
