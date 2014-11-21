80865d9cd02b4bb9cbb6719048809940eaff944c
Jason Record <g23guy@gmail.com>
2014-11-21 10:09:30 -0700

    new patterns

---
+++ patterns/SLE/sle11sp3/MozillaFirefox_SUSE-SU-2014_1385-1_11.3.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for MozillaFirefox SUSE-SU-2014:1385-1
+# Description: Security fixes for SUSE Linux Enterprise 11 SP3
+# Source:      Security Announcement Parser v1.0.6
+# Modified:    2014 Nov 21
+#
+##############################################################################
+# Copyright (C) 2014 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "MozillaFirefox"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2014-11/msg00005.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'MozillaFirefox'
+MAIN = 'MozillaFirefox'
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2014:1385-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11):
+	if ( SERVER['DistroPatchLevel'] == 3 ):
+		PACKAGES = {
+		'libfreebl3': '3.17.2-0.8.1',
+		'libfreebl3-32bit': '3.17.2-0.8.1',
+		'libfreebl3-x86': '3.17.2-0.8.1',
+		'libsoftokn3': '3.17.2-0.8.1',
+		'libsoftokn3-32bit': '3.17.2-0.8.1',
+		'libsoftokn3-x86': '3.17.2-0.8.1',
+		'MozillaFirefox': '31.2.0esr-0.14.2',
+		'MozillaFirefox-branding-SLED': '31.0-0.8.1',
+		'MozillaFirefox-branding-SLES-for-VMware': '31.0-0.3.1',
+		'MozillaFirefox-devel': '31.2.0esr-0.14.2',
+		'MozillaFirefox-translations': '31.2.0esr-0.14.2',
+		'mozilla-nspr-32bit': '4.10.7-0.3.3',
+		'mozilla-nspr': '4.10.7-0.3.3',
+		'mozilla-nspr-devel': '4.10.7-0.3.3',
+		'mozilla-nspr-x86': '4.10.7-0.3.3',
+		'mozilla-nss': '3.17.2-0.8.1',
+		'mozilla-nss-32bit': '3.17.2-0.8.1',
+		'mozilla-nss-devel': '3.17.2-0.8.1',
+		'mozilla-nss-tools': '3.17.2-0.8.1',
+		'mozilla-nss-x86': '3.17.2-0.8.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
