aad694e23552c6a21a140b159937e52902a49eeb
Jason Record <g23guy@gmail.com>
2015-06-05 13:46:27 -0600

    progress

--- patterns/SLE/sle11all/xen_mpio_lock-7011590.py
+++ patterns/SLE/sle11all/xen_mpio_lock-7011590.py
@@ -53,12 +53,19 @@ Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, O
 # Local Function Definitions
 ##############################################################################
 
-def getXenPhysicalDeviceFrom():
+def getDiskID(DEVICE_PATH):
+	"""
+	Gets the system disk (sd?) or world wide name ID for use in MPIO managed disk lookup
+	"""
+	ID = ''
+	DEV = DEVICE_PATH.split("/")[-1]
+	print "Evaluate", DEV
+	if DEV.startswith("sd"):
+		print " system"
+	return ID
 
-def getDiskFrom():
-
-def mpioManagedDevice():
-
+def mpioManagedDevice(DISK_ID):
+	return True
 
 ##############################################################################
 # Main Program Execution
@@ -70,12 +77,24 @@ if( Xen.isDom0() ):
 		MpioDevices = SUSE.mpioGetManagedDevices()
 		VM_CRIT_LIST = []
 		for XenConfig in XenConfigFiles:
-			if "phy:/dev/" in XenConfig['disk']:
-				DEVICE = getXenPhysicalDeviceFrom(XenConfig['disk'])
-				DISK = getDiskFrom(DEVICE)
-				if mpioManagedDevice(DISK):
-					VM_CRIT_LIST.append(XenConfig['name'])
-				print "\n", XenConfig
+			if "phy:/dev/" in XenConfig['disk']: #there are physical disks to process
+				DISK_LIST = Xen.getDiskValueList(XenConfig['disk'])
+				for DISK in DISK_LIST:
+					if( 'phy' in DISK['type'] ): #we only care about phy type disks
+ 						if( not DISK['mode'].endswith('!') ): #process non disk locked devices only
+							#print "Checking", DISK['device']
+							DISK_ID = getDiskID(DISK['device'])
+							if( DISK_ID ):
+								if( mpioManagedDevice(DISK_ID) ):
+									VM_CRIT_LIST.append(XenConfig['name'])
+								else:
+									print " Not managed"
+							else:
+								print " Missing ID"
+		if( len(VM_CRIT_LIST) > 0 ):
+			Core.updateStatus(Core.CRIT, "Missing disk lock or no_partition, probable boot failure for VMs: " + ' '.join(VM_CRIT_LIST))
+		else:
+			Core.updateStatus(Core.IGNORE, "No VMs using MPIO devices")
 	else:
 		Core.updateStatus(Core.ERROR, "ERROR: MPIO not active")
 else:
