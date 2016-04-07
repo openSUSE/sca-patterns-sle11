020fe0f82a22f971992f913bc9589f2b5783b1ca
Jason Record <g23guy@gmail.com>
2016-04-07 12:50:53 -0600

    new security patterns

---
+++ patterns/SLE/sle11sp2/samba_SUSE-SU-2016_0905-1_11.2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for samba SUSE-SU-2016:0905-1
+# Description: Security fixes for SUSE Linux Enterprise 11 SP2
+# Source:      Security Announcement Parser v1.3.1
+# Modified:    2016 Apr 07
+#
+##############################################################################
+# Copyright (C) 2016 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "samba"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2016-03/msg00092.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'samba'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2016:0905-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11):
+	if ( SERVER['DistroPatchLevel'] == 2 ):
+		PACKAGES = {
+			'ldapsmb': '1.34b-48.2',
+			'libldb1': '3.6.3-48.2',
+			'libsmbclient0': '3.6.3-48.2',
+			'libsmbclient0-32bit': '3.6.3-48.2',
+			'libtalloc2': '3.6.3-48.2',
+			'libtalloc2-32bit': '3.6.3-48.2',
+			'libtdb1': '3.6.3-48.2',
+			'libtdb1-32bit': '3.6.3-48.2',
+			'libtevent0': '3.6.3-48.2',
+			'libtevent0-32bit': '3.6.3-48.2',
+			'libwbclient0': '3.6.3-48.2',
+			'libwbclient0-32bit': '3.6.3-48.2',
+			'samba': '3.6.3-48.2',
+			'samba-32bit': '3.6.3-48.2',
+			'samba-client': '3.6.3-48.2',
+			'samba-client-32bit': '3.6.3-48.2',
+			'samba-debuginfo': '3.6.3-48.2',
+			'samba-debuginfo-32bit': '3.6.3-48.2',
+			'samba-debugsource': '3.6.3-48.2',
+			'samba-doc': '3.6.3-48.2',
+			'samba-krb-printing': '3.6.3-48.2',
+			'samba-winbind': '3.6.3-48.2',
+			'samba-winbind-32bit': '3.6.3-48.2',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
---
+++ patterns/SLE/sle11sp3/firefox_SUSE-SU-2016_0777-1_11.3.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for Firefox SUSE-SU-2016:0777-1
+# Description: Security fixes for SUSE Linux Enterprise 11 SP3
+# Source:      Security Announcement Parser v1.3.1
+# Modified:    2016 Apr 07
+#
+##############################################################################
+# Copyright (C) 2016 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "Firefox"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2016-03/msg00050.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'Firefox'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2016:0777-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11):
+	if ( SERVER['DistroPatchLevel'] == 3 ):
+		PACKAGES = {
+			'MozillaFirefox': '38.7.0esr-37.3',
+			'MozillaFirefox-debuginfo': '38.7.0esr-37.3',
+			'MozillaFirefox-debugsource': '38.7.0esr-37.3',
+			'MozillaFirefox-translations': '38.7.0esr-37.3',
+			'libfreebl3': '3.20.2-28.1',
+			'libfreebl3-32bit': '3.20.2-28.1',
+			'libsoftokn3': '3.20.2-28.1',
+			'libsoftokn3-32bit': '3.20.2-28.1',
+			'mozilla-nspr': '4.12-24.1',
+			'mozilla-nspr-32bit': '4.12-24.1',
+			'mozilla-nspr-debuginfo': '4.12-24.1',
+			'mozilla-nspr-debuginfo-32bit': '4.12-24.1',
+			'mozilla-nspr-debugsource': '4.12-24.1',
+			'mozilla-nss': '3.20.2-28.1',
+			'mozilla-nss-32bit': '3.20.2-28.1',
+			'mozilla-nss-debuginfo': '3.20.2-28.1',
+			'mozilla-nss-debugsource': '3.20.2-28.1',
+			'mozilla-nss-tools': '3.20.2-28.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
