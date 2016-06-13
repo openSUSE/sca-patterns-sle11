88572f0f635c25dabee05b27ec57728f7af3ff40
Jason Record <g23guy@gmail.com>
2016-06-13 13:56:46 -0600

    new expat security pattern

---
+++ patterns/SLE/sle11sp4/expat_SUSE-SU-2016_1512-1_11.4.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for expat SUSE-SU-2016:1512-1
+# Description: Security fixes for SUSE Linux Enterprise 11 SP4
+# Source:      Security Announcement Parser v1.3.1
+# Modified:    2016 Jun 13
+#
+##############################################################################
+# Copyright (C) 2016 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record (jrecord@suse.com)
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "expat"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=http://lists.opensuse.org/opensuse-security-announce/2016-06/msg00007.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = False
+NAME = 'expat'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2016:1512-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11):
+	if ( SERVER['DistroPatchLevel'] == 4 ):
+		PACKAGES = {
+			'expat': '2.0.1-88.38.1',
+			'expat-debuginfo': '2.0.1-88.38.1',
+			'expat-debugsource': '2.0.1-88.38.1',
+			'libexpat-devel': '2.0.1-88.38.1',
+			'libexpat1': '2.0.1-88.38.1',
+			'libexpat1-32bit': '2.0.1-88.38.1',
+			'libexpat1-x86': '2.0.1-88.38.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
