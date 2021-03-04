0b64a91d2016ad4504d44e17d95214ba58e3113f
Jason Record <jason.record@gmail.com>
2021-03-03 20:01:49 -0700

    Feb 2021 New Security Announcement Patterns 4

---
+++ patterns/SLE/sle11sp4/kernel_SUSE-SU-2021_14630-1_11.4.ltss.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+#
+# Title:       Important Security Announcement for Kernel SUSE-SU-2021:14630-1
+# Description: Security fixes for SUSE Linux Enterprise 11 SP4 LTSS
+# Source:      Security Announcement Parser v1.6.1
+# Modified:    2021 Mar 03
+#
+##############################################################################
+# Copyright (C) 2021 SUSE LLC
+##############################################################################
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+#  Authors/Contributors:
+#   Jason Record <jason.record@suse.com>
+#
+##############################################################################
+
+import os
+import Core
+import SUSE
+
+META_CLASS = "Security"
+META_CATEGORY = "SLE"
+META_COMPONENT = "Kernel"
+PATTERN_ID = os.path.basename(__file__)
+PRIMARY_LINK = "META_LINK_Security"
+OVERALL = Core.TEMP
+OVERALL_INFO = "NOT SET"
+OTHER_LINKS = "META_LINK_Security=https://lists.suse.com/pipermail/sle-security-updates/2021-February/008335.html"
+Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
+
+LTSS = True
+NAME = 'Kernel'
+MAIN = ''
+SEVERITY = 'Important'
+TAG = 'SUSE-SU-2021:14630-1'
+PACKAGES = {}
+SERVER = SUSE.getHostInfo()
+
+if ( SERVER['DistroVersion'] == 11):
+	if ( SERVER['DistroPatchLevel'] == 4 ):
+		PACKAGES = {
+			'kernel-bigmem': '3.0.101-108.120.1',
+			'kernel-bigmem-base': '3.0.101-108.120.1',
+			'kernel-bigmem-devel': '3.0.101-108.120.1',
+			'kernel-default': '3.0.101-108.120.1',
+			'kernel-default-base': '3.0.101-108.120.1',
+			'kernel-default-devel': '3.0.101-108.120.1',
+			'kernel-default-man': '3.0.101-108.120.1',
+			'kernel-ec2': '3.0.101-108.120.1',
+			'kernel-ec2-base': '3.0.101-108.120.1',
+			'kernel-ec2-devel': '3.0.101-108.120.1',
+			'kernel-pae': '3.0.101-108.120.1',
+			'kernel-pae-base': '3.0.101-108.120.1',
+			'kernel-pae-devel': '3.0.101-108.120.1',
+			'kernel-ppc64': '3.0.101-108.120.1',
+			'kernel-ppc64-base': '3.0.101-108.120.1',
+			'kernel-ppc64-devel': '3.0.101-108.120.1',
+			'kernel-source': '3.0.101-108.120.1',
+			'kernel-syms': '3.0.101-108.120.1',
+			'kernel-trace': '3.0.101-108.120.1',
+			'kernel-trace-base': '3.0.101-108.120.1',
+			'kernel-trace-devel': '3.0.101-108.120.1',
+			'kernel-xen': '3.0.101-108.120.1',
+			'kernel-xen-base': '3.0.101-108.120.1',
+			'kernel-xen-devel': '3.0.101-108.120.1',
+		}
+		SUSE.securityAnnouncementPackageCheck(NAME, MAIN, LTSS, SEVERITY, TAG, PACKAGES)
+	else:
+		Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the service pack scope")
+else:
+	Core.updateStatus(Core.ERROR, "ERROR: " + NAME + " Security Announcement: Outside the distribution scope")
+Core.printPatternResults()
+
